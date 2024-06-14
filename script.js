0c6ad51687bd73d2445e4b642170b6df9bc60922
arifhussain1313 <zakiriinguti@gmail.com>
2024-06-13 22:50:39 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const item = document.querySelector("#item");
+const totdobox = document.querySelector("#to-do-box");
+addEventListener ("keyup", function(event)  {
+    if(event.key == "Enter"){
+        addToDo(this.value)
+        this.value = ""
+    };
+}
+)
+const addToDo = () => {
+    const listitem = document.createElement("li");
+    listitem.innerHTML = '
+    ${item} 
+    <i class="fas fa-times"></i>';
+    listitem.addEventListener("click", function(){
+        this.classList.toggle("done");
+    })
+    listitem.querySelector("i").addEventListener ("click", function(){
+        listitem.remove ()
+    })
+    totdobox.appendChild(listitem);
+}
